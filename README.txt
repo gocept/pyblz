84ae8947d298bcac3f6a355537e4355dadbf9b2a
Wolfgang Schnerring <wosc@wosc.de>
2012-09-19 10:51:18 +0200

    Parse the BLZ file

--- README.txt
+++ README.txt
@@ -2,4 +2,28 @@
 The pyblz distribution
 ======================
 
-Wrapper around BLZ data from Deutsche Bundesbank
+Wrapper around the BLZ data from the Deutsche Bundesbank:
+http://www.bundesbank.de/Redaktion/DE/Standardartikel/Kerngeschaeftsfelder/Unbarer_Zahlungsverkehr/bankleitzahlen_download.html
+
+(The currently packaged file version is ``BLZ_20120903.txt``.)
+
+This package provides a dict of the bank information, keyed by BLZ.
+
+    >>> import pyblz
+    >>> banks = pyblz.bank_data()
+    >>> len(banks)
+    4098
+    >>> banks['64150020']
+    {'bic': u'SOLADES1TUB',
+     'blz': u'64150020',
+     'change_type': u'U',
+     'checksum_type': u'01',
+     'city': u'T\xfcbingen',
+     'delete_type': u'0',
+     'name': u'Kreissparkasse T\xfcbingen',
+     'next_blz': u'00000000',
+     'pan': u'56008',
+     'record_number': u'002683',
+     'shortname': u'Kr Spk T\xfcbingen',
+     'type': u'1',
+     'zip': u'72008'}
---
+++ src/pyblz/parse.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2012 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from pyblz.record import FixedWidthRecord
+
+
+class BLZRecord(FixedWidthRecord):
+
+    fields = [
+        ('blz', 8, ' ', FixedWidthRecord.LEFT),
+        ('type', 1, ' ', FixedWidthRecord.LEFT),
+        ('name', 58, ' ', FixedWidthRecord.LEFT),
+        ('zip', 5, ' ', FixedWidthRecord.LEFT),
+        ('city', 35, ' ', FixedWidthRecord.LEFT),
+        ('shortname', 27, ' ', FixedWidthRecord.LEFT),
+        ('pan', 5, ' ', FixedWidthRecord.LEFT),
+        ('bic', 11, ' ', FixedWidthRecord.LEFT),
+        ('checksum_type', 2, ' ', FixedWidthRecord.LEFT),
+        ('record_number', 6, ' ', FixedWidthRecord.LEFT),
+        ('change_type', 1, ' ', FixedWidthRecord.LEFT),
+        ('delete_type', 1, ' ', FixedWidthRecord.LEFT),
+        ('next_blz', 8, ' ', FixedWidthRecord.LEFT),
+    ]
+
+
+def parse(filename):
+    result = {}
+    for line in open(filename, 'r'):
+        record = BLZRecord.parse(line)
+        if record['type'] == '1':
+            result[record['blz']] = record.data
+    return result
