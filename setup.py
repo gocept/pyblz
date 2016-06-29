875c3c9ca968177c9c2ed1c9b6d5eb2ac7e74d2a
Steffen Allner <sa@gocept.com>
2016-06-29 15:59:23 +0200

    PEP-8, (c), URL and name fixes.

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2012-2013 gocept gmbh & co. kg
-# See also LICENSE.txt
-
 # This should be only one line. If it must be multi-line, indent the second
 # line onwards to keep the PKG-INFO file format intact.
 """Wrapper around BLZ data from Deutsche Bundesbank
@@ -22,20 +19,20 @@ setup(
     install_requires=[
         'setuptools',
         'gocept.recordserialize',
-        ],
+    ],
 
     extras_require={
         'test': [
-            ],
-        },
+        ],
+    },
 
     entry_points={
-        },
+    },
 
-    author='Wolfgang Schnerring <ws@gocept.com>',
-    author_email='ws@gocept.com',
+    author='gocept gmbh & co kg',
+    author_email='mail@gocept.com',
     license='ZPL 2.1',
-    url='https://projects.gocept.com/projects/pyblz/',
+    url='https://bitbucket.org/gocept/pyblz',
 
     keywords='',
     classifiers="""\
@@ -48,14 +45,14 @@ Programming Language :: Python :: 2 :: Only
 """[:-1].split('\n'),
     description=__doc__.strip(),
     long_description='\n\n'.join(open(project_path(name)).read() for name in (
-            'README.txt',
-            'HACKING.txt',
-            'CHANGES.txt',
-            )),
+        'README.txt',
+        'HACKING.txt',
+        'CHANGES.txt',
+    )),
 
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     data_files=[('', glob.glob(project_path('*.txt')))],
     zip_safe=False,
-    )
+)
